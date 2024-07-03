a2d44c66c91e664c52ec6aaff1e3dd386c698e86
Edilson <juniorse13@gmail.com>
2024-07-03 19:57:24 -0300

    importação correta do CardViewer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import StudyContext from "../contexts/study";
-import CardViewer from "../commons/CardViewer";
+import CardViewer from "./CardViewer";
 import DeckSelector from "./DeckSelector";
 
 
 const Pages = () => {
-    const { study } = useContext(StudyContext);
-    return study ? <CardViewer /> : <DeckSelector />
+	const { study } = useContext(StudyContext);
+	return study ? <CardViewer /> : <DeckSelector />
 
 }
 
