70a7b6848f2aab30187ef400d17ac5ab59d575d5
Edilson <juniorse13@gmail.com>
2024-07-03 02:40:21 -0300

    seletor de decks (inicial)

---
+++ src/pages/DeckSelector/index.tsx
@@ -0,0 +1,41 @@
+import { CSSProperties, useContext } from "react";
+import DecksJson from "../../cards/cards.json";
+import { Collapse, CollapseProps } from "antd";
+import "./styles.scss";
+import StudyContext from "../../contexts/study";
+
+export default () => {
+	const panelStyle: CSSProperties = {
+		color: "white",
+		width: "100%",
+		padding: ".5rem",
+	};
+
+	const { pickCards } = useContext(StudyContext);
+
+	const items: (panelStyle: CSSProperties) => CollapseProps["items"] = (
+		panelStyle
+	) =>
+		DecksJson.map((category, index) => ({
+			key: index,
+			label: category.name,
+			children: (
+				<ul>
+					{category.decks.map((deck) => (
+						<li key={deck.id}>
+							<button
+								className="borderless"
+								type="button"
+								onClick={() => pickCards(deck.id)}
+							>
+								{deck.name}
+							</button>
+						</li>
+					))}
+				</ul>
+			),
+			style: panelStyle,
+			headerClass: "collapse-header",
+		}));
+	return <Collapse ghost collapsible="header" items={items(panelStyle)} />;
+};
