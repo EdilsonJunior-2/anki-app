e15c930b122ae4a7db1cc3105ad5ff9ad7252fbd
Edilson <juniorse13@gmail.com>
2024-07-03 19:55:48 -0300

    CardViewer como página

--- src/pages/CardViewer/index.tsx
+++ src/pages/CardViewer/index.tsx
@@ -3,6 +3,7 @@ import "./styles.scss"
 import { changeCardRating, sendNewRatings } from "../../spacedRepetition";
 import { indexLoop } from "../../utils";
 import StudyContext from "../../contexts/study";
+import { QuestionType } from "../../commons/enums/QuestionType";
 
 export default () => {
 
@@ -22,7 +23,7 @@ export default () => {
 		showAnswer(false);
 		changeCardRating(cards[index], rating);
 		const newCardArray = cards;
-		if (cards[index].rating !== 1) {
+		if (cards[index].rating !== 4) {
 			setNewRatedCards([...newRatedCards, cards[index]]);
 			newCardArray.splice(index, 1);
 			if (index >= newCardArray.length)
@@ -34,16 +35,17 @@ export default () => {
 
 	return study ? cards.length > 0 ?
 		<div className="card-viewer">
-			<img src={cards[index].image} />
+			{cards[index].type === `${QuestionType.Image}` ? <img src={cards[index].question} /> : <p>{cards[index].question}</p>}
 			{answerFlag ? (
-				<div>
-					<div>{cards[index].answer}</div>
-					<button onClick={() => updateRating(1)}>De novo</button>
-					<button onClick={() => updateRating(2)}>Difícil</button>
-					<button onClick={() => updateRating(3)}>Médio</button>
-					<button onClick={() => updateRating(4)}>Fácil</button>
+				<div className="card-viewer-answer">
+					<p>Resposta: <span>{cards[index].answer}</span></p>
+					<div className="card-viewer-rating-options">
+						<button onClick={() => updateRating(4)}>De novo (+1min)</button>
+						<button onClick={() => updateRating(3)}>Difícil (+1 dia)</button>
+						<button onClick={() => updateRating(2)}>Médio (+4 dias)</button>
+						<button onClick={() => updateRating(1)}>Fácil (+7 dias)</button>
+					</div>
 				</div>
-
 			) : <button onClick={() => showAnswer(true)}>Ver resposta</button>}
 		</div>
 		: <div>
