4fb7059bb23c1f9865fef06de50474593190c11c
Edilson <juniorse13@gmail.com>
2024-07-03 02:41:14 -0300

    Context para salvar as cartas que estão sendo utilizadas no estudo

---
+++ src/contexts/study.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, createContext, useState } from "react";
+import { getCards } from "../spacedRepetition";
+import DecksJson from "../cards/cards.json";
+import Card from "../classes/CardClass";
+
+const StudyContext = createContext<StudyContextData>({} as StudyContextData);
+
+export const StudyProvider = ({ children }: {
+    children: ReactNode
+}) => {
+
+    const [cards, setCards] = useState<any[]>([]);
+    const [study, letTheStudyBegin] = useState<boolean>(false);
+
+    async function pickCards(id: number) {
+        await getCards(id).then((data) => {
+            const deckCards = DecksJson[data.cards[0].category - 1]
+                .decks
+                .find(d => d.id === data.deck)
+                ?.cards as any[];
+            const cardsToStudy: any[] = [];
+            deckCards.map((card: any) => {
+                data.cards.find((c: any) => {
+                    if (c.cardId === card.id) {
+                        cardsToStudy.push(
+                            new Card({ ...card, ...c })
+                        );
+                        return true;
+                    }
+                    return false;
+                });
+            });
+            setCards(cardsToStudy);
+            letTheStudyBegin(true);
+        });
+    }
+
+    return (
+        <StudyContext.Provider value={{ study, letTheStudyBegin, cards, setCards, pickCards }}>
+            {children}
+        </StudyContext.Provider>
+    )
+}
+
+export default StudyContext;
+
+interface StudyContextData {
+    study: boolean;
+    letTheStudyBegin: (study: boolean) => void;
+    cards: any[];
+    setCards: (cards: any[]) => void;
+    pickCards: (id: number) => void;
+    // updateRating(rating: number): void;
+}
