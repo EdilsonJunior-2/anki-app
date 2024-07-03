11d747bd9f88282bdb4415ddaffc400479cb5b1e
Edilson <juniorse13@gmail.com>
2024-07-03 02:40:55 -0300

    nova Class para criação de cartas

--- src/classes/CardClass.ts
+++ src/classes/CardClass.ts
@@ -1,28 +1,17 @@
-import { Card, createEmptyCard, fsrs, FSRS, FSRSParameters, generatorParameters } from "ts-fsrs";
-
-class Data {
+class Card {
+	id: number;
 	question: string;
 	answer: string;
+	rating: number;
+	schId: number;
 
-	constructor(props: Data) {
-		this.question = props.question;
-		this.answer = props.answer;
+	constructor(data: Card) {
+		this.id = data.id;
+		this.question = data.question;
+		this.answer = data.answer;
+		this.rating = data.rating;
+		this.schId = data.schId;
 	}
 }
 
-class ProjectCard extends Data {
-	id: string;
-	fsData: Card;
-	constructor(data: Data, id: string) {
-		super(data);
-		this.id = id;
-		const now = new Date();// new Date();
-		const card: Card = createEmptyCard(now);
-		const params: FSRSParameters = generatorParameters({ enable_fuzz: true, maximum_interval: 10000 });
-
-		const f: FSRS = new FSRS(params);
-		this.fsData = card;
-	}
-}
-
-export default ProjectCard;
+export default Card;
