4c47069165944ae64d8ec11b46cad17f408e9ac7
Edilson <juniorse13@gmail.com>
2024-08-23 16:22:56 -0300

    https

--- src/commons/api/index.ts
+++ src/commons/api/index.ts
@@ -3,7 +3,7 @@ import DeckApi from "./decks";
 import AuthApi from "./auth";
 
 const api = axios.create({
-  baseURL: "terrible-clementia-thatsmytcc-10e74d42.koyeb.app/",
+  baseURL: "https://terrible-clementia-thatsmytcc-10e74d42.koyeb.app/",
   //"https://terrible-clementia-thatsmytcc-10e74d42.koyeb.app/"
   // "https://anki-backend-gajj.onrender.com"
   //"http://localhost:3000"
