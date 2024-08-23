737c6bb3b512e37d40042cf8f57e8b6312d1c871
Edilson <juniorse13@gmail.com>
2024-08-23 16:18:33 -0300

    url correta do backend

--- src/commons/api/index.ts
+++ src/commons/api/index.ts
@@ -1,17 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 import DeckApi from "./decks";
 import AuthApi from "./auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
-    //"https://terrible-clementia-thatsmytcc-10e74d42.koyeb.app/"
-    // "https://anki-backend-gajj.onrender.com"
-    //"http://localhost:3000"
-})
+  baseURL: "terrible-clementia-thatsmytcc-10e74d42.koyeb.app/",
+  //"https://terrible-clementia-thatsmytcc-10e74d42.koyeb.app/"
+  // "https://anki-backend-gajj.onrender.com"
+  //"http://localhost:3000"
+});
 
 export default api;
 
-export {
-    AuthApi,
-    DeckApi
-}
+export { AuthApi, DeckApi };
