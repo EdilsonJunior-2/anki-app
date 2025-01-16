f535a89a53b887b6ac2bf37775173b593b03d25f
Edilson <juniorse13@gmail.com>
2025-01-16 18:45:36 +0100

    creating student admin flag

--- src/commons/classes/student.ts
+++ src/commons/classes/student.ts
@@ -2,11 +2,13 @@ class Student {
   id: number;
   name: string;
   code: string;
+  admin: boolean;
 
   constructor(props: Student) {
     this.name = props.name;
     this.code = props.code;
     this.id = props.id;
+    this.admin = props.admin;
   }
 }
 
