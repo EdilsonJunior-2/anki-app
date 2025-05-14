a6e304f039c86fad33c61dc409c4ccd23da0706f
Edilson <juniorse13@gmail.com>
2025-05-14 12:48:35 -0300

    trocas de rota usando apenas useNavigate

--- src/pages/Admin/Home/index.tsx
+++ src/pages/Admin/Home/index.tsx
@@ -3,9 +3,10 @@ import type { TableColumnsType } from "antd";
 import { Table } from "antd";
 import { AdminApi } from "@api";
 import { formatDateByTimestamp } from "@utils";
-import { TopBar } from "@components";
+import { ProjectText, TopBar } from "@components";
 import { StudentContext } from "@context";
 import "./styles.scss";
+import { useNavigate } from "react-router";
 
 interface DataType {
   key: React.Key;
@@ -14,29 +15,37 @@ interface DataType {
   name: string;
 }
 
-const columns: TableColumnsType<DataType> = [
-  { title: "Matrícula", dataIndex: "code", key: "code" },
-  { title: "Nome", dataIndex: "name", key: "name" },
-  {
-    title: "Último registro de estudo",
-    dataIndex: "study_timestamp",
-    key: "study_timestamp",
-    render: (s) => (
-      <span style={{ color: "#000000" }}>
-        {s ? formatDateByTimestamp(Number(s)) : "---"}
-      </span>
-    ),
-  },
-  {
-    title: "",
-    key: "operation",
-    render: (s) => <a href={`/admin/studentDetails/${s.code}`}>Detalhes</a>,
-  },
-];
-
 export default () => {
   const { student } = useContext(StudentContext);
   const [students, setStudents] = useState<any>([]);
+  const navigate = useNavigate();
+
+  const columns: TableColumnsType<DataType> = [
+    { title: "Matrícula", dataIndex: "code", key: "code" },
+    { title: "Nome", dataIndex: "name", key: "name" },
+    {
+      title: "Último registro de estudo",
+      dataIndex: "study_timestamp",
+      key: "study_timestamp",
+      render: (s) => (
+        <span style={{ color: "#000000" }}>
+          {s ? formatDateByTimestamp(Number(s)) : "---"}
+        </span>
+      ),
+    },
+    {
+      title: "",
+      key: "operation",
+      render: (s) => (
+        <span
+          style={{ color: "#000000", cursor: "pointer" }}
+          onClick={() => navigate(`/admin/studentDetails/${s.code}`)}
+        >
+          Detalhes
+        </span>
+      ),
+    },
+  ];
 
   useEffect(() => {
     AdminApi.getStudentsData(student?.code as string).then((res) => {
@@ -47,8 +56,12 @@ export default () => {
   return (
     <main className="admin-home-container">
       <TopBar title={`Bem vindo, ${student?.name.split(" ", 2)[0]}`} />
+
       <div className="table-container">
         <Table<DataType> columns={columns} dataSource={students} />
+        <ProjectText>
+          <span onClick={() => navigate("/student")}>Modo Estudante</span>
+        </ProjectText>
       </div>
     </main>
   );
