3c2a61f7bdf7c22f48504a8f4012d1c987477837
Edilson <juniorse13@gmail.com>
2025-06-28 14:43:34 -0300

    Fix: brain favicon

--- src/commons/components/TopBar/index.tsx
+++ src/commons/components/TopBar/index.tsx
@@ -59,7 +59,6 @@ export default (props: TopBarProps) => {
 
   return (
     <Header>
-      <img src="/brain.png" alt="brain" />
       <ProjectText headerLevel={3}>{props.title}</ProjectText>
       {!props.hideOptions && chapters && (
         <Menu
